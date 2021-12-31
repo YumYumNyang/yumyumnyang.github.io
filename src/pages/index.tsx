5fcc04194c8b820cfaea86d08bd3d32dba5c82d2
YumYumNyang <khw121699@gmail.com>
2021-12-31 17:21:03 +0900

    rename: 폴더 정리 및 추가

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'gatsby'
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+      <Link to="/about">to about</Link>
+    </div>
+  )
+}
+
+export default index
