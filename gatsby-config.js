5df42422cd8e1f67a07c52880e55c96a0f754e63
YumYumNyang <khw121699@gmail.com>
2022-01-08 10:36:16 +0900

    chore: modify gatsby-config.js (meta config)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,7 @@
+const gatsbyMetaConfig = require("./gatsby-meta-config");
+
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
+  siteMetadata: gatsbyMetaConfig,
   plugins: [
     {
       resolve: "gatsby-plugin-typescript",
