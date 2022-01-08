59dc421b3fbf581b61192d5b0af9fed821da39f9
YumYumNyang <khw121699@gmail.com>
2022-01-08 10:34:30 +0900

    chore: add gatsby-meta-config

---
+++ gatsby-meta-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: `Hyewon Kwak(yumyumnyang) tech blog`,
+  description: `얌얌냥이의 기술 블로그`,
+  siteURL: `https://yumyumnyang.github.io/`, // blog site URL
+  author: {
+    name: `곽혜원`,
+    description: `지속적인 성장을 추구하는 개발자`,
+    sns: {
+      github: `https://github.com/YumYumNyang`,
+      email: `khw121699@gmail.com`
+    },
+  },
+  icon: ``,
+  keywords: [`blog`, `얌얌냥이`, `블로그`]
+  // 나중에 추가해야할 것 : utterance, google analytics와 다른 세부 설정들(이미지, 프로젝트 페이지 등)
+}
